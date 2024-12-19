d40b81ad32dca4a4a6cfd60827f22e322def50c5
Aniket <annudubey1026@gmail.com>
2024-12-20 02:48:08 +0530

    redux initial setup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "../lib/redux/store";
+import { Providers } from "@/lib/redux/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Provider store={store}>
+    <Providers>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
           {children}
-        </Provider>
-      </body>
-    </html>
+        </body>
+      </html>
+    </Providers >
   );
 }
--- client/src/lib/redux/store.ts
+++ client/src/lib/redux/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import studentReducer from './studentSlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        students: studentReducer
+    },
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
